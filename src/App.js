37d366e95f75651ed8bc1791d2a729186bed7263
freehk <novookoss@gmail.com>
2019-07-19 01:15:54 +0200

    display story cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+class Square extends React.Component {
+  render() {
+    var squareStyle = {
+      height: 150,
+      backgroundColor: this.props.picture
+    };
+
+    return <div style={squareStyle} />;
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    var labelStyle = {
+      fontFamily: "sans-serif",
+      fontWeight: "bold",
+      padding: 13,
+      margin: 0
+    };
+
+    return <p style={labelStyle}>{this.props.story}</p>;
+  }
+}
+
+class StoryCard extends React.Component {
+  render() {
+    var cardStyle = {
+      height: 200,
+      width: 150,
+      padding: 50,
+      display: "inline-block",
+      WebkitFilter: "drop-shadow(0px 0px 5px #666)",
+      filter: "drop-shadow(0px 0px 5px #666)"
+    };
+
+    return (
+      <div style={cardStyle}>
+        <Square picture={this.props.picture} />
+        <Label story={this.props.story} />
+      </div>
+    );
+  }
+}
+var colors = [
+  "#393E41",
+  "#E94F37",
+  "#1C89BF",
+  "#A1D363",
+  "#85FFC7",
+  "#297373",
+  "#FF8552",
+  "#A40E4C"
+];
+var renderData = colors.map(value => (
+  <StoryCard key={value} picture={value} story="bullshit" />
+));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Free Hong Kong!</p>
       </header>
+      <div className="App-body">{renderData}</div>
     </div>
   );
 }
