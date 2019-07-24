bf84ae445a82a494b41fb0babbd9cef1b050900c
freehk <novookoss@gmail.com>
2019-07-24 12:57:37 +0200

    include link to imgur

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -15,7 +15,15 @@ class Contact extends Component {
             <h4>Thank you for caring about Hong Kong!</h4>
             <p>
               If you would like to send us any pictures, stories, or anything at
-              all, please contact us at novookoss at gmail.
+              all, please contact us at novookoss at gmail. Or post directly at{" "}
+              <a
+                href="https://imgur.com/t/freehongkong"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                imgur
+              </a>
+              &nbsp;with the tag <strong>freehongkong</strong>
             </p>
           </div>
         </div>
