e4123d6ef9c95f958628ce663294220843c4d71c
freehk <novookoss@gmail.com>
2019-07-28 11:09:50 +0200

    sorting and filtering by date

--- src/Components/Action.js
+++ src/Components/Action.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import sortByDate from "../utils/sortByDate";
 
 var trStyle = {
   textAlign: "left",
@@ -18,31 +19,36 @@ class Action extends Component {
     if (this.props.data) {
       var headers = [
         <th style={trStyle} key="date">
-          {" "}
           Date
         </th>,
         <th style={trStyle} key="location">
-          {" "}
           Location
         </th>,
         <th style={trStyle} key="event">
-          {" "}
           Event
         </th>
       ];
-      var actions = this.props.data.map(value => {
-        return (
-          <tr key={value.title} style={trStyle}>
-            <td style={tdStyle}>{value.date}</td>
-            <td style={tdStyle}>{value.location} </td>
-            <td style={tdStyle}>
-              <a href={value.link} target="_blank">
-                <span>{value.title}</span>
-              </a>
-            </td>
-          </tr>
-        );
-      });
+      var actions = this.props.data
+        .filter(d => {
+          return (
+            new Date(d["date"]).getTime() >=
+              new Date().getTime() - 24 * 60 * 60 * 1000 || d["date"] == ""
+          );
+        })
+        .sort(sortByDate("date", "asc"))
+        .map(value => {
+          return (
+            <tr key={value.title} style={trStyle}>
+              <td style={tdStyle}>{value.date}</td>
+              <td style={tdStyle}>{value.location} </td>
+              <td style={tdStyle}>
+                <a href={value.link} target="_blank">
+                  <span>{value.title}</span>
+                </a>
+              </td>
+            </tr>
+          );
+        });
     }
 
     return (
@@ -52,7 +58,7 @@ class Action extends Component {
             <h1>What can you do to help us?</h1>
             <table style={tableStyle}>
               <tbody>
-                <tr>{headers} </tr>
+                <tr>{headers}</tr>
                 {actions}
               </tbody>
             </table>
